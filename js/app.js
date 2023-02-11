256b517c66c9fce79eeb50e777e84210c930e8ca
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2023-02-10 22:18:11 -0800

    :heavy_plus_sign: function to return desired date

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const newBeginnings = '04 June 2023';
+
+function countDown() {
+    const newDate = new Date(newBeginnings);
+    const currentDate = new Date();
+
+    console.log(newDate - currentDate);
+}
+
+countDown();
